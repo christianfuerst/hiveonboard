744ea4a1b1f8d82519dac7ec040136c0acd802ed
roomservice <roomservice@yourbtc.net>
2020-05-10 18:59:21 +0200

    minor layout fix for mobile

--- src/routes/LandingPage/index.js
+++ src/routes/LandingPage/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
 
 import { landingContent } from "../../config";
 import LandingCard from "../../components/LandingCard";
 
 const useStyles = makeStyles((theme) => ({
   grid: {
-    margin: 10,
+    margin: 0,
     padding: theme.spacing(1),
   },
 }));
@@ -20,13 +21,19 @@ const LandingPage = () => {
       className={classes.grid}
       container
       direction="row"
-      justify="space-between"
+      justify="center"
       alignItems="center"
     >
       {landingContent.map((element, index) => {
         return (
           <Grid item xs={12} sm={4} key={index}>
-            <LandingCard content={element} />
+            <Box
+              display="flex"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <LandingCard content={element} />
+            </Box>
           </Grid>
         );
       })}
